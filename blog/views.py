c67eaa7e26c715fe8b34cab8c0a97a943780cfab
João Givago Coradin <joao@autoconf.com.br>
2021-06-16 18:07:30 -0300

    add project

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from typing import List
+from django.views.generic import DetailView, ListView
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
